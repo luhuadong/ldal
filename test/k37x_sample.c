a76121f0a03c45614e81f1ea42775db0aeeeb6b9
Rudy Lo <luhuadong@163.com>
2021-07-21 01:20:38 +0800

    update device table example

--- test/k37x_sample.c
+++ test/k37x_sample.c
@@ -1,33 +1,23 @@
 #include <stdio.h>
 #include "ldal.h"
 
-/* The second parameter pass <remote_ip:port> or <netdev> */
-static struct ldal_udp_device udp0 = {
-    "udp_server", "0.0.0.0",
-};
-
-static struct ldal_rtc_device rtc0 = {
-    "rtc0", "/dev/rtc0",
-};
-
 static struct ldal_device_table device_table[] = {
-    { "AI0", "/dev/aidev0", LDAL_CLASS_ANALOG, 0 },
-    { "AI1", "/dev/aidev1", LDAL_CLASS_ANALOG, 1 },
-    { "AI2", "/dev/aidev2", LDAL_CLASS_ANALOG, 2 },
-    { "AI3", "/dev/aidev3", LDAL_CLASS_ANALOG, 3 },
-    { "AI4", "/dev/aidev4", LDAL_CLASS_ANALOG, 4 },
-    { "AI5", "/dev/aidev5", LDAL_CLASS_ANALOG, 5 },
-    { "AI6", "/dev/aidev6", LDAL_CLASS_ANALOG, 6 },
-    { "AI7", "/dev/aidev7", LDAL_CLASS_ANALOG, 7 },
-    { "AI8", "/dev/aidev8", LDAL_CLASS_ANALOG, 8 },
-    { "AI9", "/dev/aidev9", LDAL_CLASS_ANALOG, 9 },
-    { "AI10", "/dev/aidev10", LDAL_CLASS_ANALOG, 10 },
-    { "AI11", "/dev/aidev11", LDAL_CLASS_ANALOG, 11 },
-    { "AI12", "/dev/aidev12", LDAL_CLASS_ANALOG, 12 },
-    { "AI13", "/dev/aidev13", LDAL_CLASS_ANALOG, 13 },
-    { "AI14", "/dev/aidev14", LDAL_CLASS_ANALOG, 14 },
-    { "AI15", "/dev/aidev15", LDAL_CLASS_ANALOG, 15 },
+    { "udp_server", "0.0.0.0", LDAL_CLASS_UDP },
+
+    { "tcp0", "127.0.0.1:8888", LDAL_CLASS_TCP },
+    { "tcp1", "127.0.0.1:8888", LDAL_CLASS_TCP },
+    { "tcp2", "127.0.0.1:8888", LDAL_CLASS_TCP },
+    { "tcp3", "127.0.0.1:8888", LDAL_CLASS_TCP },
+    { "tcp4", "127.0.0.1:8888", LDAL_CLASS_TCP },
+    { "tcp5", "127.0.0.1:8888", LDAL_CLASS_TCP },
+    { "tcp6", "127.0.0.1:8888", LDAL_CLASS_TCP },
+    { "tcp7", "127.0.0.1:8888", LDAL_CLASS_TCP },
+
+    { "BT",   "/dev/ttymxc2", LDAL_CLASS_SERIAL },
+    { "EC25", "/dev/ttyUSB6", LDAL_CLASS_ME },
     
+    { "RTC0", "/dev/rtc0", LDAL_CLASS_RTC },
+
     { "DI0", "/dev/didev0", LDAL_CLASS_DIGITAL },
     { "DI1", "/dev/didev1", LDAL_CLASS_DIGITAL },
     { "DI2", "/dev/didev2", LDAL_CLASS_DIGITAL },
@@ -45,6 +35,28 @@ static struct ldal_device_table device_table[] = {
     { "DO5", "/dev/dodev5", LDAL_CLASS_DIGITAL },
     { "DO6", "/dev/dodev6", LDAL_CLASS_DIGITAL },
     { "DO7", "/dev/dodev7", LDAL_CLASS_DIGITAL },
+
+    { "AI0", "/dev/aidev0", LDAL_CLASS_ANALOG, 0 },
+    { "AI1", "/dev/aidev1", LDAL_CLASS_ANALOG, 1 },
+    { "AI2", "/dev/aidev2", LDAL_CLASS_ANALOG, 2 },
+    { "AI3", "/dev/aidev3", LDAL_CLASS_ANALOG, 3 },
+    { "AI4", "/dev/aidev4", LDAL_CLASS_ANALOG, 4 },
+    { "AI5", "/dev/aidev5", LDAL_CLASS_ANALOG, 5 },
+    { "AI6", "/dev/aidev6", LDAL_CLASS_ANALOG, 6 },
+    { "AI7", "/dev/aidev7", LDAL_CLASS_ANALOG, 7 },
+    { "AI8", "/dev/aidev8", LDAL_CLASS_ANALOG, 8 },
+    { "AI9", "/dev/aidev9", LDAL_CLASS_ANALOG, 9 },
+    { "AI10", "/dev/aidev10", LDAL_CLASS_ANALOG, 10 },
+    { "AI11", "/dev/aidev11", LDAL_CLASS_ANALOG, 11 },
+    { "AI12", "/dev/aidev12", LDAL_CLASS_ANALOG, 12 },
+    { "AI13", "/dev/aidev13", LDAL_CLASS_ANALOG, 13 },
+    { "AI14", "/dev/aidev14", LDAL_CLASS_ANALOG, 14 },
+    { "AI15", "/dev/aidev15", LDAL_CLASS_ANALOG, 15 },
+
+    { "RS485_0", "/dev/ttyUSB0", LDAL_CLASS_SERIAL },
+    { "RS485_1", "/dev/ttyUSB0", LDAL_CLASS_SERIAL },
+    { "RS485_2", "/dev/ttyUSB0", LDAL_CLASS_SERIAL },
+    { "RS485_3", "/dev/ttyUSB0", LDAL_CLASS_SERIAL },
 
     {"ibutton", "/dev/k37adev_ibutton", LDAL_CLASS_MISC },
     {"battery", "/dev/k37xdev_battery", LDAL_CLASS_MISC },
@@ -65,18 +77,8 @@ int main(int argc, char *argv[])
 
     printf("K37X sample -- register %lu devices\n", ARRAY_SIZE(device_table));
 
-    ldal_device_create(&device_table, ARRAY_SIZE(device_table));
-
     /* Register device */
-    ret = ldal_device_register(&udp0.device, udp0.device_name, udp0.file_name, LDAL_CLASS_UDP, (void *)&udp0);
-    if (ret != LDAL_EOK) {
-        printf("Register udp device failed\n");
-    }
-
-    ret = ldal_device_register(&rtc0.device, rtc0.device_name, rtc0.file_name, LDAL_CLASS_RTC, (void *) &rtc0);
-    if (ret != LDAL_EOK) {
-        printf("Register rtc device failed\n");
-    }
+    ldal_device_create(&device_table, ARRAY_SIZE(device_table));
 
     ldal_show_device_list();
     establish_serve();
